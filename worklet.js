0447e01c6922d3012afd14ddf9e8021b30399682
Sage Jenson <andrescuervor@gmail.com>
2018-07-13 14:48:29 +0200

    Add finished necker cube demo!

--- worklet.js
+++ worklet.js
@@ -1,38 +1,55 @@
-class DancingBarsPainter {
+class NeckerCubePainter {
     // Typed OM
     static get inputProperties() {
-        return ['--bar-color', '--side-length'];
+        return [
+            '--side-length',
+            '--box-color',
+        ];
     }
 
     paint(ctx, geom, props) {
         const lineWidth = geom.width * 0.01;
 
+        ctx.beginPath();
         ctx.lineWidth = lineWidth;
         ctx.strokeStyle = props.get('--box-color');
         let sideLength = props.get('--side-length');
 
-        console.log(props.get('--side-length'))
-
+        let x = geom.width * 0.1
+        let y = x
+        let length = geom.width * 0.35
         let points = [
-            geom.width * 0.1,
-            geom.width * 0.1,
-            geom.width * 0.5,
-            geom.width * 0.5,
+            x,
+            y,
+            length,
+            length
         ]
 
         ctx.rect(...points)
 
-        let shiftedPoints = []
-
-        for (let i = 0; i < 4; ++i) {
-            let newPoint = points[i] + (sideLength * 10)
-            shiftedPoints.push(newPoint)
-        }
-
+        let shiftedPoints = points.map(oldPoint => oldPoint + sideLength * 10)
         ctx.rect(...shiftedPoints)
+        let x_prime = shiftedPoints[0]
+        let y_prime = shiftedPoints[1]
+        let length_prime = shiftedPoints[2]
 
+        // Top left stroke
+        ctx.moveTo(x, y);
+        ctx.lineTo(x_prime, y_prime);
+
+        // Top right stroke
+        ctx.moveTo(x + length, y);
+        ctx.lineTo(x_prime + length_prime, y_prime);
+
+        // Bottom left stroke
+        ctx.moveTo(x, y + length);
+        ctx.lineTo(x_prime, y_prime + length_prime);
+
+        // Bottom right stroke
+        ctx.moveTo(x + length, y + length);
+        ctx.lineTo(x_prime + length_prime, y_prime + length_prime);
         ctx.stroke()
     }
 }
 
-registerPaint('dancing-bars', DancingBarsPainter);
+registerPaint('necker-cube', NeckerCubePainter);
